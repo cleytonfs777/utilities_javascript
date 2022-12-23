31812bb2fcee8b362e650972e310a740acf47fd3
cleytonfs777 <91349547+cleytonfs777@users.noreply.github.com>
2022-12-23 18:43:55 -0300

    update math_a.js

--- math_a.js
+++ math_a.js
@@ -1,1 +1,14 @@
+let numb = 9.60
+// Arredonda o número para cima
+// let numb2 = Math.ceil(numb)
+// Arredonda o número para baixo
+//let numb2 = Math.floor(numb)
+// Arredonda o número para baixo se menor que .50
+//let numb2 = Math.round(numb)
+// numero aleatório entre 0 e 1 excluimdo o 0
+// let numb2 = Math.random()
+// Se quisermos um aleatório entre num1 e num2, sendo num1 o maior e num2 o menor temos:
+// let numb2 = Math.random()*(num1-num2) + num2
+let numb2 = Math.round(Math.random()*(60-1) + 1)
 
+console.log(numb2)
