f8718a4e51a80e40db2d12f7a048df3a4c2ffc74
cleytonfs777 <91349547+cleytonfs777@users.noreply.github.com>
2022-12-24 12:17:29 -0300

    update curto_circuito.js

---
+++ curto_circuito.js
@@ -0,0 +1,15 @@
+/*
+&& => false && true => false - retorna sempre o valor falso, exatamente o valor. Em uma lista de valores o primeiro falso que achar ele retorna, se não tiver nenhum ele retorna o último verdadeiro
+
+|| => false || true => true - retorna o primeiro valor verdadeiro. O primeiro verdadeiro que achar retorna. Caso contrário, ele retorna o último falso
+
+Falsy Values
+*false
+* 0 '' "" ``
+* null undefined
+* NaN
+
+
+
+
+*/
