2761aaf462320a22fc6dc763a07058c011c43de8
cleytonfs777 <91349547+cleytonfs777@users.noreply.github.com>
2022-12-24 13:44:44 -0300

    update lanc_trat_error.js

--- lanc_trat_error.js
+++ lanc_trat_error.js
@@ -1,1 +1,21 @@
-
+function soma(a, b){
+  if(typeof(a) !== 'number' || typeof(b) !== 'number'){
+// Lançamento de erro apenas com string
+ //   throw('a e b obrigatóriamente devem ser numeros')
+// Lançamento de erro tipado
+  throw new Error('a e b obrigatóriamente devem ser numeros')
+  }
+  return a + b
+}
+//Tratamento de erros
+try{
+  console.log(soma(2,6))
+}catch(e){
+  /* Não se recomenda lançar erros na tela 
+  console.log(e)*/
+  console.log("Algo saiu errado")
+}
+// Esse bloco sempre será executado
+finally{
+  console.log(" Independente do que ocorer eu to aqui")
+}
